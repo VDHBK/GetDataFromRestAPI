4ac16a77d24fcb34fe5053d7192242267ec479fa
ヴー ドゥック フィー <CXJ772@muratec.co.jp>
2021-01-05 15:56:57 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React, { Component } from "react";
-
-//import "bootstrap/dist/css/bootstrap.min.css";
-
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      users: [],
+      results: [],
     };
   }
 
@@ -15,40 +12,51 @@ class App extends Component {
     await fetch("http://jsonplaceholder.typicode.com/users")
       .then((res) => res.json())
       .then((data) => {
-        this.setState({ users: data });
+        this.setState({ results: data });
       })
       .catch(console.log);
   }
 
-  renderUsers = () => {
-    let users = this.state.users.map((data, index) => (
+  renderResults = () => {
+    let results = this.state.results.map((data, index) => (
       <tr key={data.id}>
         <td>{data.id}</td>
         <td>{data.name}</td>
         <td>{data.email}</td>
+        <td>{data.address.zipcode}</td>
+        <td>{data.address.city}</td>
         <td>{data.website}</td>
       </tr>
     ));
 
-    return users;
+    return results;
   };
 
   render() {
     return (
       <div className="App">
         <div className="container">
-          <h1 className="text-center">Users List</h1>
+          <h1 className="text-center">Results List</h1>
 
           <table className="table">
             <thead>
               <tr>
-                <th>#</th>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Website</th>
+                <th>AGV</th>
+                <th>更新</th>
+                <th>モード</th>
+                <th>状態</th>
+                <th>バッテリー</th>
+                <th>在荷</th>
+                <th>昼夜</th>
+                <th>上位制御</th>
+                <th>現在地</th>
+                <th>次中継</th>
+                <th>目的地</th>
+                <th>タスク</th>
+                <th>経路</th>
               </tr>
             </thead>
-            <tbody>{this.renderUsers()}</tbody>
+            <tbody>{this.renderResults()}</tbody>
           </table>
         </div>
       </div>
